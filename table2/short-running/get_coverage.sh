ed4ec624cb8fdace6fafaedc6ef6e34df89b929b
RuiqiGao <gao606@purdue.edu>
2023-01-27 22:45:22 -0500

    add short-running script

---
+++ table2/short-running/get_coverage.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+declare -a orders
+
+gcov_dir=$1
+
+klee_replay_bin=klee-replay
+
+orders+=(echo)
+orders+=(cat)
+orders+=(base32)
+orders+=(base64)
+orders+=(comm)
+orders+=(cut)
+orders+=(dirname)
+orders+=(expand)
+orders+=(true)
+orders+=(fold)
+orders+=(join)
+orders+=(link)
+orders+=(paste)
+orders+=(pathchk)
+
+curr_dir=$(pwd)
+
+cd ${gcov_dir}
+
+for id in "${!orders[@]}"; do
+  program=${orders[$id]}
+  rm -f *.gcda
+  gcov_name=${program}
+  if [ $program = "base32" ] || [ $program = "base64"  ]; then
+  	gcov_name=${program}-basenc
+  fi
+  #echo $gcov_name
+  ${klee_replay_bin} ./${program} ${curr_dir}/gs/${program}-tests/*.ktest
+  gcov $gcov_name > gs_${program}_gcov.log
+  cp ./gs_${program}_gcov.log ${curr_dir}/gs/${program}_gcov.log
+done
+
+
+for id in "${!orders[@]}"; do
+  program=${orders[$id]}
+  rm -f *.gcda
+  gcov_name=${program}
+  if [ $program = "base32" ] || [ $program = "base64"  ]; then
+  	gcov_name=${program}-basenc
+  fi
+  #echo $gcov_name
+  ${klee_replay_bin} ./${program} ${curr_dir}/klee/klee-${program}-0/*.ktest
+  gcov $gcov_name > klee_${program}_gcov.log
+  cp ./klee_${program}_gcov.log ${curr_dir}/klee/${program}_gcov.log
+done
+
+
+cd ${curr_dir}
---
+++ table2/short_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+gcov_src_dir=/icse23/coreutils-src/obj-gcov/src
+
+cd short-running
+bash test.sh
+bash data_process.sh
+bash get_coverage.sh ${gcov_src_dir}
+python3 plot.py
+cp ./short-running.csv ../
+cd ..
+
+#'
